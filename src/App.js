a7b7808fd5804cadb622b3ca0c48b0be73d82e6b
Antoine de Prieelle <antoine_@live.nl>
2020-12-19 13:35:00 +0100

    Add responsive grid template areas layout to app.js

--- src/App.js
+++ src/App.js
@@ -15,9 +15,63 @@ class App extends Component {
       <div>
         <Header />
         <div className="components-wrapper">
-          <GeneralInformation />
+          {/* <GeneralInformation />
           <EducationalExperience />
-          <WorkExperience />
+          <WorkExperience /> */}
+          {/* <div className="contact-education-expertise"> */}
+            <div className="contact">
+              <div className="profile-pic">profile-pic</div>
+              <div className="contact-header-options">
+                <div className="contact-header">Contact</div>
+                <div className="contact-options">
+                  <div className="phone">phone</div>
+                  <div className="mail">mail</div>
+                  <div className="location">location</div>
+                  <div className="linkedIn">linkedIn</div>
+                </div>
+              </div>
+            </div>
+            <div className="education">
+              <div className="education-header">Education</div>
+              <div className="education-degrees">
+                <div className="degree">degree</div>
+                <div className="uni">uni</div>
+                <div className="year">year</div>
+              </div>
+            </div>
+            <div className="expertise">
+              <div className="expertise-header">Expertise</div>
+              <div className="expertise-skills">
+                <div className="expertise-skill">team work</div>
+                <div className="expertise-skill">project management</div>
+              </div>
+            </div>
+          {/* </div> */}
+          {/* <div className="name-profile-professional"> */}
+            <div className="name-function">
+              <div className="name">Name</div>
+              <div className="function">function</div>
+            </div>
+            <div className="profile-description">
+              <div className="profile-header">Profile</div>
+              <div className="profile-text">profile-text</div>
+            </div>
+            <div className="professional-experience">
+              <div className="professional-experience-header">Professional experience</div>
+              <div className="professional-experience-jobs">
+                <div className="professional-experience-job">
+                  <div className="job-title">job title</div>
+                  <div className="company-location-date">
+                    <div className="company">company</div>
+                    <div className="location">location</div>
+                    <div className="date">date</div>
+                  </div>
+                  <div className="job-description">job description</div>
+                </div>
+              </div>
+            </div>
+          {/* </div> */}
+
         </div>
       </div>
     )
