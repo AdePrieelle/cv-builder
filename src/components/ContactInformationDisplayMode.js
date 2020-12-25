aa8e36f2c3a7641cb83345479d931a22a4faa923
Antoine de Prieelle <antoine_@live.nl>
2020-12-25 14:27:24 +0100

    Add contact icons to react component ContactInformationDisplayMode

--- src/components/ContactInformationDisplayMode.js
+++ src/components/ContactInformationDisplayMode.js
@@ -1,20 +1,38 @@
 import React from "react";
+import "../styles/ContactInformationDisplayMode.scss"
+
 
 const ContactInformationDisplayMode = (props) => {
   return (
     <div className="component">
-        <div className="component-header-display-mode">Contact</div>
+        <div className="component-header-display-mode-contact">Contact</div>
         <div className="component-props">
           {props.inputFields.map((inputField, index) => (
                 <div key={index}>
-                  <div className="form-label">Phone</div>
-                  <div className="props-value">{inputField.inputPhone}</div>
-                  <div className="form-label">Mail</div>
-                  <div className="props-value">{inputField.inputMail}</div>
-                  <div className="form-label">Location</div>
-                  <div className="props-value">{inputField.inputLocation}</div>
-                  <div className="form-label">LinkedIn</div>
-                  <div className="props-value">{inputField.inputLinkedIn}</div>
+                  <div className="contact-option">
+                    <div className="contact-icon-container">
+                      <i class="fas fa-phone-alt contact-icon"></i>
+                    </div>
+                    <div className="props-value">{inputField.inputPhone}</div>
+                  </div>
+                  <div className="contact-option">
+                    <div className="contact-icon-container">
+                      <i class="fas fa-envelope contact-icon"></i>
+                    </div>
+                    <div className="props-value">{inputField.inputMail}</div>
+                  </div>
+                  <div className="contact-option">
+                    <div className="contact-icon-container">
+                      <i class="fas fa-home contact-icon"></i>
+                    </div>
+                    <div className="props-value">{inputField.inputLocation}</div>
+                  </div>
+                  <div className="contact-option">
+                    <div className="contact-icon-container">
+                      <i class="fab fa-linkedin-in contact-icon"></i>
+                    </div>
+                    <div className="props-value">{inputField.inputLinkedIn}</div>
+                  </div>
                 </div>
             ))}
         </div>
